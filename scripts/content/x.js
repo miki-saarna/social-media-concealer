ef3fad58ad5c31c6f131fc62e919246214482cc5
Miki Saarna <86010458+miki-saarna@users.noreply.github.com>
2024-05-13 18:35:21 -0700

    Init x concealer

---
+++ scripts/content/x.js
@@ -0,0 +1,40 @@
+(() => {
+  hideElements();
+
+  // document.addEventListener("yt-navigate-finish", () => {
+  //   hideElements();
+  // });
+})();
+
+function hideElements() {
+  removeHider();
+
+  const url = window.location.href;
+
+  if (url === "https://twitter.com/home") {
+    const css = `
+      .css-175oi2r.r-f8sm7e.r-13qz1uu.r-1ye8kvj {
+        visibility: hidden !important;
+      }
+
+      .css-175oi2r.r-vacyoi.r-ttdzmv > :not(:first-child) {
+        visibility: hidden !important;
+        background: red;
+      }
+    `;
+
+    const style = document.createElement("style");
+    style.id = "x_content_concealer";
+    style.appendChild(document.createTextNode(css));
+    document.head.appendChild(style);
+  }
+
+  // if (url.includes("results")) {}
+}
+
+function removeHider() {
+  const styleElement = document.head.querySelector("#x_content_concealer");
+  if (styleElement) {
+    styleElement.remove();
+  }
+}
